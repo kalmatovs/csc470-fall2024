816aa8f6ad0b92333920f894d38db87008d46f54
kalmatovs <sk7772b@student.american.edu>
2024-09-10 14:02:53 -0400

    first push

---
+++ in-progress/My project2/Assets/NewSphereScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class NewSphereScript : MonoBehaviour
+{
+
+    public TMP_Text scoreText;
+    public Rigidbody rb;
+    int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.useGravity = true;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        Debug.Log("Hello?");
+        Destroy (other.gameObject);
+        score++;
+        if(score > 3) {
+            scoreText.text = "You win";
+        } else {
+            scoreText.text = "Score: " + score;
+        }
+    }
+
+    public void CollisionEnter (Collision col) {
+
+    }
+}
